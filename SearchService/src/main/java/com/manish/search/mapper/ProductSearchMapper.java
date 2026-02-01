0ef69cbbd71935c915501d93eeae011f778357e2
manish <ms5978307@gmail.com>
2026-02-01 16:05:14 +0530

    feat: Implement search functionality in ProductSearchService and add GetProductCompactDTO mapping

--- SearchService/src/main/java/com/manish/search/mapper/ProductSearchMapper.java
+++ SearchService/src/main/java/com/manish/search/mapper/ProductSearchMapper.java
@@ -1,5 +1,6 @@
 package com.manish.search.mapper;
 
+import com.manish.search.dto.GetProductCompactDTO;
 import com.manish.search.entity.ProductSearchDocument;
 import com.manish.search.event.ProductUpsertEvent;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,15 @@ public class ProductSearchMapper {
                 .tags(event.getTags())
                 .category(event.getCategory())
                 .price(event.getPrice())
+                .build();
+    }
+
+    public static GetProductCompactDTO toCompact(ProductSearchDocument productSearchDocument) {
+        return GetProductCompactDTO.builder()
+                .id(productSearchDocument.getProductId())
+                .name(productSearchDocument.getName())
+                .price(productSearchDocument.getPrice())
+                .category(productSearchDocument.getCategory())
                 .build();
     }
 }
--- SearchService/src/main/java/com/manish/search/service/ProductSearchService.java
+++ SearchService/src/main/java/com/manish/search/service/ProductSearchService.java
@@ -1,4 +1,40 @@
 package com.manish.search.service;
 
+import com.manish.search.dto.GetProductCompactDTO;
+import com.manish.search.entity.ProductSearchDocument;
+import com.manish.search.mapper.ProductSearchMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.*;
+import org.springframework.data.elasticsearch.core.query.StringQuery;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProductSearchService {
+
+    private final ElasticsearchOperations elasticsearchOperations;
+
+    public Page<GetProductCompactDTO> search(String q, Pageable pageable) {
+
+        String jsonQuery = """
+        {
+          "multi_match": {
+            "query": "%s",
+            "fields": ["name^3", "description", "tags"]
+          }
+        }
+        """.formatted(q);
+
+        StringQuery query = new StringQuery(jsonQuery);
+        query.setPageable(pageable);
+
+        SearchHits<ProductSearchDocument> hits =
+                elasticsearchOperations.search(query, ProductSearchDocument.class);
+
+        return SearchHitSupport.searchPageFor(hits, pageable)
+                .map(SearchHit::getContent)
+                .map(ProductSearchMapper::toCompact);
+    }
 }
